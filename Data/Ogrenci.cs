1d68263c18ceca37a8f3e439192205e4922b9728
afurkankoc <139684794+ahmedfurkankoc@users.noreply.github.com>
2023-10-16 13:28:12 +0300

    update

--- Data/Ogrenci.cs
+++ Data/Ogrenci.cs
@@ -4,8 +4,11 @@ namespace efcoreApp.Data
 {
     public class Ogrenci
     {
-        [Key] //Key / Primary Key => By convention, a property named Id or <type name>Id will be configured as the primary key of an entity.
-        public int OgrenciKimlik{ get; set; }
-        public int MyProperty { get; set; }
+        [Key] //Key / Primary Key => By convention, a property named Id or <type name>Id will be configured as the primary key of an entity. Another way it needs to be marked with KEY annotation.
+        public int OgrenciKimlik { get; set; }
+        public string? OgrenciAd { get; set; }
+        public string? OgrenciSoyad { get; set; }
+        public string? Email { get; set; }
+        public string? Telefon  { get; set; }
     }
 }
