76114906acc0bfe0d9c306bfe014d7c886ee7ba8
Ahmed Furkan <ahmedfurkankoc@gmail.com>
2023-10-16 15:18:47 +0300

    update project

--- Controllers/OgrenciController.cs
+++ Controllers/OgrenciController.cs
@@ -32,7 +32,83 @@ namespace efcoreApp.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
-    }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var ogr = await _context.Ogrenciler.FindAsync(id); // FindAsync => sadece id ye göre bir arama yapılabilir.
+            
+            // var ogr = await _context.Ogrenciler.FirstOrDefaultAsync(o => o.OgrenciKimlik == id); // o.OgrenciKimlik kısmına örnek olarak Telefon yazılırsa telefon numarası ile de arama yapılabilir.
+
+
+            if(ogr == null)
+            {
+                return NotFound();
+            }
+
+            return View(ogr);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // (Cross Site Attack) form içerisine asp.net'in eklemiş olduğu RequestVerificationToken value'sının kontrolunu yapmasını sağlar. 
+        public async Task<IActionResult> Edit(int id, Ogrenci model)
+        {
+            if(id != model.OgrenciKimlik)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model); //güncelleme işaretlendi
+                    await _context.SaveChangesAsync(); //güncelleme yapıldı.
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    if(!_context.Ogrenciler.Any(o => o.OgrenciKimlik == model.OgrenciKimlik))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
     
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var ogrenci = await _context.Ogrenciler.FindAsync(id);
+
+            if(ogrenci == null)
+            {
+                return NotFound();
+            }
+
+            return View(ogrenci);
+        } 
+
+        [HttpPost]
+        public async Task<IActionResult> Delete()
+        {
+            
+        }
+        
+    }    
 }
